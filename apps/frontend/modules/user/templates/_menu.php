87d50ff1305fc9bd86cfc99c0b70dfa811dd5e1c
Djuro <gazer60@gmail.com>
2012-07-03 02:51:50 +0200

    Izbrisan nepotreban fajl.

--- apps/frontend/modules/user/templates/_menu.php
+++
@@ -1,9 +0,0 @@
-<div id="menu">
-				<ul>
-					<li><a href="<?php echo url_for('home/index')?>">Homepage</a></li>
-					<li><a href="#">Gallery</a></li>
-					<li><a href="#">About</a></li>
-					<li><a href="#">Resources</a></li>
-					<li><a href="<?php echo url_for('user/index')?>">Register</a></li>
-				</ul>
-			</div>
